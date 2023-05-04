a1750fefd20a58606f5b8bd527965780476edec3
kanha-13 <kanha.agr11@gmail.com>
2023-05-04 23:58:45 +0530

    modified backend

---
+++ Face-Count/Server/SendImage.py
@@ -0,0 +1,55 @@
+# import requests
+# import pickle
+# import numpy as np
+# import json
+
+# myurl = 'https://localhost:3001/processimage'
+# url = "http://localhost:3001/input"
+
+# files = {'image': open('img1.jpg', 'rb')}
+
+
+# image_file = 'img1.jpg'
+
+# with open(image_file, "rb") as f:
+#     im_bytes = f.read()
+# im_b64 = base64.b64encode(im_bytes).decode("utf8")
+
+# headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+# payload = json.dumps({"image": im_b64, "other_key": "value"})
+
+
+# test_res = requests.post(myurl,files = files)
+# if test_res.ok:
+#     print(" File uploaded successfully ! ")
+#     print(test_res.text)
+
+# else:
+#     print(" Please Upload again ! ")
+
+# # from os.path import exists
+
+# # file_exists = exists('input/img1.jpg')
+# # print(file_exists)
+
+
+import requests
+import cv2
+import sys
+className = sys.argv[1]
+
+cam = cv2.VideoCapture(0)
+
+result, image = cam.read()
+
+imagePath = "../input/"+"class-"+className+".jpg"
+cv2.imwrite(imagePath, image)
+# dfile = open("classA.jpg", "rb")
+# url = "http://localhost:3001/processimage"
+# test_res = requests.post(url, files = {"classimage": dfile})
+# if test_res.ok:
+#     print(" File uploaded successfully ! ")
+#     print(test_res.text)
+# else:
+#     print(" Please Upload again ! ")
--- Face-Count/Server/server.js
+++ Face-Count/Server/server.js
@@ -5,38 +5,91 @@ const app = express()
 const fs = require('fs')
 const { exec } = require('child_process')
 const PORT = process.env.PORT || 3001
+const dotenv = require("dotenv")
+const class_attandence = require("./db")
+dotenv.config()
 
 //middleware
 app.use(cors())
 app.use(express.json());
 app.use(filUpload());
 
+
+//db connection
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log("Connected to DB")
+}).catch(err => {
+    console.log(err)
+})
+
 //api endpoints
 app.get('/', (req, res) => {
-    res.status(200).send("Hola")
+    // res.status(200).send("Hola")
+    req.send('index.html')
 })
 
-app.post('/processimage', (req, res) => {
-    const image = req.files.classimage
-    console.log(image)
-    fs.writeFileSync(`../input/${image.name}`, image.data, function (err) {
-        if (err) {
-            return console.log(err);
-        }
+app.get('/getCount/:class', async (req, res) => {
+    captureImage(req.params.class);
+    await processImage(res, req.params.class).then((count) => {
+    }).catch(() => {
+        res.status(500).send("Uable to process request!")
     });
-    console.log("Processning")
-    exec('cd .. ; python3 face-count.py',(err,stdout)=>{
-        console.log("Processing done")
-        console.log(stdout)
-    })
-    res.status(201).send("Image updated successfully")
 })
 
-app.get('/studentcount',(req,res)=>{
-    const stringdata = fs.readFileSync("../count.json");
-    const parsedData = JSON.parse(stringdata);
-    res.json(parsedData);
+app.get('/getCount/all', async (req, res) => {
+    captureImage(0);
+    await processImage(res, 0).then((count) => {
+    }).catch(() => {
+        res.status(500).send("Uable to process request!")
+    });
 })
+
+
+
+// app.get('/studentcount', (req, res) => {
+//     const stringdata = fs.readFileSync("../count.json");
+//     const parsedData = JSON.parse(stringdata);
+//     res.json(parsedData);
+// })
 
 //listener
 app.listen(PORT, () => console.log(`Server running at ${PORT}`))
+
+
+
+// drivers
+
+const captureImage = (clas) => {
+    if (clas === 0) {
+        // capture image for every class
+    }
+    else
+        exec(`python3 SendImage.py ${clas}`, (err, stdout) => {
+            console.log("Image captured successfully 👍")
+        })
+}
+
+const processImage = async (res, clas) => {
+    exec('cd .. ; python3 face-count.py', (err, stdout) => {
+        console.log("Processing done")
+        saveCount(clas, stdout)
+        console.log(stdout)
+        res.status(200).json({ class: clas, count: stdout })
+    })
+}
+
+const saveCount = async (classname, count) => {
+    const data = {
+        className: classname,
+        count: count,
+        date: new Date()
+    }
+    const new_count = new class_attandence(data)
+    await new_count.save().then(() => {
+        console.log("updated db successfully 👍")
+    })
+}
